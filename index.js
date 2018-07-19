27ba8d2841c4830b9dde23af99510be87c4fe991
cglim <cheegan@hotmail.com>
2018-07-19 11:06:42 +0800

    commit message goes here

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var config = require('./config/config.json'),
-    server = require('./lib/server');
+    //server = require('./lib/server');
+    server = require('./lib/common_room_server');
 
 config.PORT = process.env.PORT || config.PORT;
 
